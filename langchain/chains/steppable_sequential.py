bbbf943f984674ac6d14010b3259198f5c3b7902
musnit <musnit@gmail.com>
2023-04-23 00:52:27 -0700

    clean logs

--- langchain/chains/steppable_sequential.py
+++ langchain/chains/steppable_sequential.py
@@ -12,7 +12,6 @@ class SteppableSequentialChain(SequentialChain):
     known_values = {}
     current_chain = 0
     is_running = asyncio.Event()
-    # pause_scheduled = False
 
     class NoMoreChainsError(Exception):
         pass
@@ -21,42 +20,23 @@ class SteppableSequentialChain(SequentialChain):
         pass
 
     async def pause(self) -> None:
-        print("pausing func start")
         if self.current_chain == len(self.chains):
             return
-        # self.pause_scheduled = True
         self.is_running.clear()
         self.paused = True
-        print("pausing func end")
 
     async def play(self) -> None:
-        print("playing func start")
         if self.current_chain == len(self.chains):
             return
-        # self.pause_scheduled = True
         self.is_running.set()
         self.paused = False
-        print("playing func end")
 
     async def stepper(self) -> Dict[str, str]:
         while self.current_chain < len(self.chains):
-            print(f"is running set: {self.is_running.is_set}")
             await self.is_running.wait()
             self.step()
-            # if self.pause_scheduled:
-            #     self.paused = True
-            #     self.pause_event.set()
-            #     self.pause_scheduled = False
-            print(f"completed subchain step {self.current_chain - 1}")
-            print(f"waiting 0.5 secs before next step")
             await asyncio.sleep(0.5)
 
-        print("chain completed")
-        print(f"output_variables: {self.output_variables}")
-        print(f"known_values: {self.known_values}")
-        for k in self.output_variables:
-            print(f"k: {k}")
-            print(f"known_values[k]: {self.known_values[k]}")
         return {k: self.known_values[k] for k in self.output_variables}
 
     def step(self) -> None:
@@ -73,7 +53,6 @@ class SteppableSequentialChain(SequentialChain):
         """Run the logic of this chain and return the output."""
         self.known_values = inputs.copy()
         self.current_chain = 0
-        print("kickoff chain")
         await self.play()
         return await self.stepper()
 
